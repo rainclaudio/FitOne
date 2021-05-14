267edfe1bb4b1df3b356c6a81fd2293c4b0208d7
Claudio Javier Rain Levicán <rainclaudio25@gmail.com>
2021-05-14 09:06:29 -0400

    día 1 -- services and food !

---
+++ src/app/table/foodNode.model.ts
@@ -0,0 +1,10 @@
+export interface foodNode{
+  id:string;
+  calories: number;
+  carbohydrates: number;
+  fat: number;
+  protein: number;
+  sodium: number;
+  sugar: number;
+
+}
--- src/app/table/table.page.ts
+++ src/app/table/table.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { foodNode } from './foodNode.model';
+import { TableService } from './table.service';
 
 @Component({
   selector: 'app-table',
@@ -6,10 +8,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./table.page.scss'],
 })
 export class TablePage implements OnInit {
+  private copyFoodVector: foodNode[] = [];
+  private choosedFood: foodNode[] = [];
 
-  constructor() { }
+  counterCalories: number;
+  private counterProtein: number;
+  private counterCarbohydrates: number;
+  private counterFat: number;
+  private counterSodium: number;
+  private counterSugar: number;
+
+  constructor(private tableservice: TableService) {}
 
   ngOnInit() {
+    this.copyFoodVector = this.tableservice.get_allFoods();
   }
+  getVectorCopy() {
+    return [...this.copyFoodVector];
+  }
+  getChoosedFood() {
+    return [...this.choosedFood];
+  }
+  addFood(id: string) {
 
+    this.choosedFood.push(
+      this.copyFoodVector.find((food) => {
+        return food.id === id;
+      })
+    );
+  }
 }
